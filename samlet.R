0294a3d378f7889ee3c240cf1c52972629c13300
evablichertryde <76792836+evablichertryde@users.noreply.github.com>
2021-12-21 21:49:20 +0100

    Add files via upload

---
+++ samlet.R
@@ -0,0 +1,6 @@
+##how to change error message lanuage
+Sys.setenv("LANGUAGE"="EN")
+library(tidyverse)
+library(foreign)
+
+df_humanvalue <-read.csv("C:/Users/Bruger/OneDrive - Københavns Universitet/speciale/data behandling ess 2014/df_humanvalue.csv")
